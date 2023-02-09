912c655860d175fad989f515a6144416b7dac280
jo suhyun <josu23@naver.com>
2023-02-09 15:33:35 +0900

    최종

--- proj_crawling.py
+++ proj_crawling.py
@@ -1,19 +1,19 @@
 import requests 
 from bs4 import BeautifulSoup
+import pyautogui
 
-keyword = input("주제 : ")
-page = int(input("원하는 페이지 수 : "))
+keyword = pyautogui.prompt("검색어를 입력하세요 >> ")
+page = pyautogui.prompt("원하는 페이지 수 : ")
 
-
-for i in range(0,page) :
+for i in range(0,int(page)) :
     response = requests.get(f"https://search.naver.com/search.naver?where=news&sm=tab_pge&query={keyword}&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=32&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start={i*10+1}")
+    print(f"{i+1}페이지 입니다. =====================================================================================")
     html = response.text
-
     soup = BeautifulSoup(html,'html.parser')
-    links = soup.select(".news_tit") #결과는 리스트
+    links = soup.select(".news_tit")
 
     for link in links:
-        title = link.text # 태그 안에 텍스트 요소를 가져온다.
-        url = link.attrs['href'] #href의 속성값을 가져온다.
+        title = link.text
+        url = link.attrs['href']
         print(title,url)
     i+= 10
