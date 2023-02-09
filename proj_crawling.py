5dad154c6e8e1f2a030fa7ccc88450b5f7f1b6cd
jo suhyun <josu23@naver.com>
2023-02-09 15:35:22 +0900

    최종

--- proj_crawling.py
+++ proj_crawling.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pyautogui
 
 keyword = pyautogui.prompt("검색어를 입력하세요 >> ")
-page = pyautogui.prompt("원하는 페이지 수 : ")
+page = pyautogui.prompt("원하는 페이지 수 >> ")
 
 for i in range(0,int(page)) :
     response = requests.get(f"https://search.naver.com/search.naver?where=news&sm=tab_pge&query={keyword}&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=32&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start={i*10+1}")
